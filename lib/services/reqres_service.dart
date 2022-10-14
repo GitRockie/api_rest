44373b2020d830fdde4ae0aa784352bae74f5d6d
GitRockie <elromankos@gmail.com>
2022-10-14 11:06:24 +0200

    create personModel variable. catchError with log

--- lib/services/reqres_service.dart
+++ lib/services/reqres_service.dart
@@ -1,8 +1,10 @@
 import 'dart:convert';
+import 'dart:developer';
 import 'package:api_rest/models/models.dart';
 import 'package:http/http.dart' as http;
 
 class ServiceRequestApi {
+  PersonsModel personsModel = PersonsModel();
   void getReqRespService() {
     //Parseamos Url
     final url = Uri.parse('https://reqres.in/api/users?page=2#');
@@ -10,10 +12,16 @@ class ServiceRequestApi {
     print('awesome: Ya tengo mi Api!!');
     print(url);
     http.get(url).then((response) {
-      print(response);
+      print(response.body);
 
-      final reqRes = PersonsModel.fromJson(jsonDecode(response.body));
-      print(reqRes);
+      final personsModel = PersonsModel.fromJson(jsonDecode(response.body));
+      print(personsModel.data);
+      print(personsModel.data!.first.email);
+      personsModel.data?.forEach((element) {
+        print(element.firstName);
+      });
+    }).catchError((err) {
+      log('$err');
     });
   }
 }
